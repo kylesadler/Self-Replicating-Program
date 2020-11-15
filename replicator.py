21bd1e756312ed6ed8bd2ed44b326c4cc143aef9
kyle sadler <kylesadler5@gmail.com>
2020-11-14 23:34:10 -0600

    better comments

--- replicator.py
+++ replicator.py
@@ -1,28 +1,28 @@
-# a
+# Part A: write Part B's code to a new file
 from datetime import datetime
 TAPE = f'replicator{datetime.now().strftime("%s")}.py'
 with open(TAPE, 'w') as f:
     f.write("""
-# b
+# Part B: use Part A's output to rerwite the program that is currently running
 from datetime import datetime
 TAPE = f'replicator{datetime.now().strftime(\"%s\")}.py'
 with open(TAPE, 'r') as f:
     program = f.read()
 with open(TAPE, 'w') as f:
-    f.write(\"\"\"# a
+    f.write(\"\"\"# Part A: write Part B's code to a new file
 from datetime import datetime
 TAPE = f'replicator{datetime.now().strftime(\\\"%s\\\")}.py'
 with open(TAPE, 'w') as f:
     f.write(\\\"\\\"\\\"\"\"\"+str(program.replace('\\\\','\\\\\\\\').replace('\"','\\\\\"'))+\"\"\"\\\"\\\"\\\")
 \"\"\" + program)""")
 
-# b
+# Part B: use Part A's output to rerwite the program that is currently running
 from datetime import datetime
 TAPE = f'replicator{datetime.now().strftime("%s")}.py'
 with open(TAPE, 'r') as f:
     program = f.read()
 with open(TAPE, 'w') as f:
-    f.write("""# a
+    f.write("""# Part A: write Part B's code to a new file
 from datetime import datetime
 TAPE = f'replicator{datetime.now().strftime(\"%s\")}.py'
 with open(TAPE, 'w') as f:
