28e457b5149235fa7c1ed08e9c1cc5ca62b5abbb
kyle sadler <kylesadler5@gmail.com>
2020-11-14 23:25:42 -0600

    init

--- replicator.py
+++ replicator.py
@@ -9,14 +9,12 @@ TAPE = f'replicator{datetime.now().strftime(\"%s\")}.py'
 with open(TAPE, 'r') as f:
     program = f.read()
 with open(TAPE, 'w') as f:
-    f.write(\"\"\"
-# a
+    f.write(\"\"\"# a
 from datetime import datetime
 TAPE = f'replicator{datetime.now().strftime(\\\"%s\\\")}.py'
 with open(TAPE, 'w') as f:
-    f.write(\\\"\\\"\\\"\"\"\"+str(program)+\"\"\"\\\"\\\"\\\")
-\"\"\" + program)
-""")
+    f.write(\\\"\\\"\\\"\"\"\"+str(program.replace('\\\\','\\\\\\\\').replace('\"','\\\\\"'))+\"\"\"\\\"\\\"\\\")
+\"\"\" + program)""")
 
 # b
 from datetime import datetime
@@ -24,60 +22,9 @@ TAPE = f'replicator{datetime.now().strftime("%s")}.py'
 with open(TAPE, 'r') as f:
     program = f.read()
 with open(TAPE, 'w') as f:
-    f.write("""
-# a
+    f.write("""# a
 from datetime import datetime
 TAPE = f'replicator{datetime.now().strftime(\"%s\")}.py'
 with open(TAPE, 'w') as f:
-    f.write(\"\"\""""+str(program)+"""\"\"\")
+    f.write(\"\"\""""+str(program.replace('\\','\\\\').replace('"','\\"'))+"""\"\"\")
 """ + program)
-
-
-
-
-
-
-
-
-
-# from datetime import datetime
-# TAPE = f'replicator{datetime.now().strftime("%s")}.py'
-
-# # a
-# with open(TAPE, 'w') as f:
-#     f.write("""
-# # b
-# from datetime import datetime
-# TAPE = f'replicator{datetime.now().strftime("%s")}.py'
-# with open(TAPE, 'r') as f:
-#     program = f.read()
-
-# with open(TAPE, 'w') as f:
-#     f.write(\"\"\"
-# with open(TAPE, 'w') as f:
-#     f.write(\\\"\\\"\\\"
-# from datetime import datetime
-# TAPE = f'replicator{datetime.now().strftime(\\\"%s\\\")}.py'
-# with open(TAPE, 'w') as f:
-#     f.write(\"\"\"+str(program)+\"\"\")
-# \\\"\\\"\\\")
-# \"\"\" + program)
-# """)
-
-# # b
-# from datetime import datetime
-# TAPE = f'replicator{datetime.now().strftime("%s")}.py'
-# with open(TAPE, 'r') as f:
-#     program = f.read()
-# with open(TAPE, 'w') as f:
-#     f.write("""
-# from datetime import datetime
-# TAPE = f'replicator{datetime.now().strftime(\"%s\")}.py'
-# with open(TAPE, 'w') as f:
-#     f.write(\"\"\"
-# from datetime import datetime
-# TAPE = f'replicator{datetime.now().strftime(\\\"%s\\\")}.py'
-# with open(TAPE, 'w') as f:
-#     f.write("""+str(program)+""")
-# \"\"\")
-# """ + program)
